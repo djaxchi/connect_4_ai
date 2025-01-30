ac08387739dca8cd6b317d807ef2369209a444c6
yolaatar <laatarjo@gmail.com>
2025-01-30 08:58:42 +0100

    mamacita

--- c4/c4WithAI.pl
+++ c4/c4WithAI.pl
@@ -1,18 +1,16 @@
 % Initialise un plateau vide
 init_board(Board) :-
-    Board = [[e, e, e, e, e, e],
-             [e, e, e, e, e, e],
-             [e, e, e, e, e, e],
-             [e, e, e, e, e, e],
-             [e, e, e, e, e, e],
-             [e, e, e, e, e, e],
-             [e, e, e, e, e, e]].
+    Board = [[e, e, e, e, e, e, e],
+             [e, e, e, e, e, e, e],
+             [e, e, e, e, e, e, e],
+             [e, e, e, e, e, e, e],
+             [e, e, e, e, e, e, e],
+             [e, e, e, e, e, e, e]].
 
 % Affiche le plateau
 display_board(Board) :-
-    transpose(Board, DisplayableBoard),
     nl, write('  1   2   3   4   5   6   7'), nl,
-    maplist(display_row, DisplayableBoard), nl.
+    maplist(display_row, Board), nl.
 
 
 display_row(Row) :-
@@ -21,17 +19,18 @@ display_row(Row) :-
     nl.
 
 display_cell(Cell) :-
-    (Cell = e -> write(' e '); write(' '), write(Cell), write(' ')),
+    (Cell = e -> write('   '); write(' '), write(Cell), write(' ')),
     write('|').
 
 % Insère un jeton dans une colonne
 insert_in_column(Board, Col, Mark, NewBoard) :-
-    nth1(Col, Board, Column), % Récupérer la colonne spécifiée
+    transpose(Board, Transposed), % Transposer pour accéder aux colonnes
+    nth1(Col, Transposed, Column), % Récupérer la colonne spécifiée
     reverse(Column, ReversedColumn), % Commencer par le bas
     replace_first_empty(ReversedColumn, Mark, UpdatedReversedColumn),
     reverse(UpdatedReversedColumn, UpdatedColumn), % Restaurer l'ordre
-    replace_column(Board, Col, UpdatedColumn, NewBoard). % Mettre à jour la colonne
-
+    replace_column(Transposed, Col, UpdatedColumn, UpdatedTransposed), % Mettre à jour la colonne
+    transpose(UpdatedTransposed, NewBoard). % Re-transposer pour restaurer le format original
 
 replace_first_empty([e|Rest], Mark, [Mark|Rest]).
 replace_first_empty([Cell|Rest], Mark, [Cell|UpdatedRest]) :-
@@ -60,37 +59,63 @@ vertical_win(Board, Mark) :-
     consecutive_four(Column, Mark).
 
 diagonal_win(Board, Mark) :-
-    diagonals(Board, Diagonals),
-    member(Diagonal, Diagonals),
+    diagonals_desc(Board, DescDiagonals),
+    diagonals_asc(Board, AscDiagonals),
+    append(DescDiagonals, AscDiagonals, AllDiagonals),
+    member(Diagonal, AllDiagonals),
     consecutive_four(Diagonal, Mark).
 
-diagonals(Board, Diagonals) :-
-    findall(Diagonal, diagonal(Board, Diagonal), Diagonals).
+diagonals_desc(Board, Diagonals) :-
+    findall(Diagonal, diagonal_desc(Board, Diagonal), Diagonals).
 
-diagonal(Board, Diagonal) :-
-    nth1(StartRow, Board, _),
-    nth1(StartCol, [1,2,3,4,5,6,7], _),
-    extract_diagonal(Board, StartRow, StartCol, Diagonal).
+diagonal_desc(Board, Diagonal) :-
+    length(Board, NumRows),
+    length(Board, NumCols),
+    (   between(1, NumRows, StartRow), StartCol = 1  % Diagonales partant de la première colonne
+    ;   StartRow = 1, between(2, NumCols, StartCol)  % Diagonales partant de la première ligne
+    ),
+    extract_diagonal_desc(Board, StartRow, StartCol, Diagonal).
 
-extract_diagonal(_, Row, Col, []) :-
-    Row < 1 ; Col < 1 ; Row > 6 ; Col > 7, !.
-extract_diagonal(Board, Row, Col, [Elem|Diagonal]) :-
+extract_diagonal_desc(_, Row, Col, []) :-
+    Row > 6 ; Col > 7, !.  % Hors limites
+extract_diagonal_desc(Board, Row, Col, [Elem|Diagonal]) :-
     nth1(Row, Board, Line),
     nth1(Col, Line, Elem),
     NextRow is Row + 1,
     NextCol is Col + 1,
-    extract_diagonal(Board, NextRow, NextCol, Diagonal).
+    extract_diagonal_desc(Board, NextRow, NextCol, Diagonal).
+
+diagonals_asc(Board, Diagonals) :-
+    findall(Diagonal, diagonal_asc(Board, Diagonal), Diagonals).
+
+diagonal_asc(Board, Diagonal) :-
+    length(Board, NumRows),
+    length(Board, NumCols),
+    (   between(1, NumRows, StartRow), StartCol = 1  % Diagonales partant de la première colonne
+    ;   StartRow = NumRows, between(2, NumCols, StartCol)  % Diagonales partant de la dernière ligne
+    ),
+    extract_diagonal_asc(Board, StartRow, StartCol, Diagonal).
+
+extract_diagonal_asc(_, Row, Col, []) :-
+    Row < 1 ; Col > 7, !.  % Hors limites
+extract_diagonal_asc(Board, Row, Col, [Elem|Diagonal]) :-
+    nth1(Row, Board, Line),
+    nth1(Col, Line, Elem),
+    NextRow is Row - 1,
+    NextCol is Col + 1,
+    extract_diagonal_asc(Board, NextRow, NextCol, Diagonal).
+
 
 consecutive_four(List, Mark) :-
     append(_, [Mark, Mark, Mark, Mark|_], List).
 
 % Vérifie si une colonne est valide
 valid_column(Board, Col) :-
-    nonvar(Board),                         % Vérifie que le plateau est instancié
+    transpose(Board, TransposedBoard),
+    nonvar(TransposedBoard),                         % Vérifie que le plateau est instancié
     between(1, 7, Col),                    % Génère les colonnes valides (1 à 7)
-    nth1(Col, Board, Column),              % Récupère la colonne correspondante
+    nth1(Col, TransposedBoard, Column),              % Récupère la colonne correspondante
     member(e, Column).                     % Vérifie s'il y a une case vide dans la colonne
-
 
 % Boucle principale
 play :-
@@ -100,8 +125,6 @@ play :-
     display_board(Board),
     play_turn(Board, x, Difficulty). % Transmet le niveau de difficulté
 
-
-% Tour de jeu
 % Tour de jeu
 play_turn(Board, Player, Difficulty) :-
     (   Player = x
@@ -145,7 +168,6 @@ random_ai_move(Board, Player, NewBoard) :-
     ValidMoves \= [],                             % Vérifie qu'il existe des colonnes valides
     random_member(Col, ValidMoves),              % Choisit une colonne au hasard
     insert_in_column(Board, Col, Player, NewBoard).
-
 
 
 % Inverse le joueur
